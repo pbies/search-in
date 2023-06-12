9d944ff0b97a58ebdd6b6468d5a7d241ecde5736
Piotr Biesiada <pmbiesiada@gmail.com>
2023-06-12 04:46:01 +0200

    init

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace search_in
+{
+    internal class Program
+    {
+        static int Main(string[] args)
+        {
+            if (args.Length < 2)
+            {
+                Console.WriteLine("Use: search-in patterns.txt search-in.txt");
+                return 1;
+            }
+
+            var patternsFile = args[0];
+            var searchFile = args[1];
+
+            var pat = File.ReadAllLines(patternsFile, Encoding.UTF8);
+
+            int i = 0;
+
+            using (var reader = new StreamReader(searchFile))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    foreach (var p in pat)
+                    {
+                        if (line.StartsWith(p))
+                        {
+                            Console.WriteLine(line);
+                        }
+                    }
+                    if (i % 1000 == 0)
+                    {
+                        Console.Error.Write($"\r");
+                        Console.Error.Write(string.Format("{0:N0}", i));
+                    }
+                    i++;
+                }
+                Console.Error.Write($"\r");
+                Console.Error.Write(string.Format("{0:N0}", i));
+                Console.Error.Write($"\a");
+            }
+
+            Console.Error.WriteLine("End of program");
+            return 0;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# search-in
- Search for patterns in file
+	Search for patterns in file
+
+	Use:
+
+	search-in file1 file2 > file3
+
+	Where:
+
+	file1 - file with patterns to search for (mostly public addresses, one per line); this file is loaded into memory
+	file2 - file with public addresses # private keys # brainwallets - in this file the patterns will be searched using StartsWith function (beginning of line, makes it fast); this file is read line by line
+	file3 - file with result - found file1 lines in file2, file2 line is printed
+
+	Program prints progress (number of lines processed) to standar error output.
